d9c9e6755f452f933568ae1d720bba39a5f33b02
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-14 19:53:35 -0400

    Longest Word (coderbyte)

---
+++ LongestWord.js
@@ -0,0 +1,25 @@
+// Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. 
+
+// Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+function LongestWord(sen) { 
+
+    // code goes here 
+    let longestNum = 0;
+    let longestWord = null;
+  
+    
+    let wordsOnly = sen.match(/[a-z]+/gi);
+    let answer = wordsOnly.forEach(word => {
+        if(word.length > longestNum) {
+            longestNum = word.length;
+            longestWord = word;
+              
+        }
+    });
+    return longestWord; 
+           
+  }
+     
+  // keep this function call here 
+  LongestWord(readline());
