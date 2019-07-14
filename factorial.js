368479cea21e0de52c3d95816b96a4da89ad0f2f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-13 22:09:14 -0400

    First Factorial(coderByte challenge)

---
+++ factorial.js
@@ -0,0 +1,12 @@
+function FirstFactorial(num) { 
+
+    // code goes here  
+    if (num === 0) {
+        return 1
+    } else {
+      return num * FirstFactorial(num - 1);    
+    }           
+  }
+     
+  // keep this function call here 
+  FirstFactorial(readline());
