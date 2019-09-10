2f76d5bff4e347fb1fde1173abfa1c8c37a24cad
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-09 23:19:57 -0400

    HackerRank Challenge Plus Minus. Completed with Python - updated with challenge details

--- plus_minus.py
+++ plus_minus.py
@@ -1,3 +1,5 @@
+# Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+
 import math
 import os
 import random
