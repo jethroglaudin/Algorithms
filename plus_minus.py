e003def5346d2084141788e7f4e359dc0340f06a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-09 23:19:36 -0400

    HackerRank Challenge Plus Minus. Completed with Python

---
+++ plus_minus.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    length = len(arr)
+    pos = 0
+    neg = 0
+    zero = 0
+ 
+
+    for num in arr:
+        if num > 0:
+            pos += 1  
+
+        elif num < 0:
+            neg += 1  
+        else:
+            zero += 1
+             
+
+    print("{:.6f}".format(pos / length))
+    print("{:.6f}".format(neg / length))
+    print("{:.6f}".format(zero / length))
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
