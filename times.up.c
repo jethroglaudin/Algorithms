8d6020e993ede4eaac930f4b7434a9cdc4bfcb5a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-10 23:43:57 -0400

    completed constraints. starting hour 1 <= x <= 12 and time pased int_min<= y<= int_max

--- times.up.c
+++ times.up.c
@@ -8,7 +8,6 @@ int main(void)
     /*
     Given a starting Hour and a number of hours passed display the following
     ex. The time is now x:00 and y days have passed
-
     constrainsts  
     starting hour 1 <= x <= 12 
     time pased 1<= y<= int_max
@@ -16,34 +15,33 @@ int main(void)
     y the number we increment x the time by. time is now 8:00
     days would be 0
     */
-   unsigned int *temp;
-   
-   char temp2[1];
-   bool condition = true;
+   int temp;
+   int temp2;
+ 
 
    printf("What time is it currently: ");
-   scanf("%d", temp);
+   scanf("%d", &temp);
    
-   unsigned int current = *(int*)temp;
+   unsigned int current = *(int*)&temp;
    
-   if(current <= 0 || current >= 13)
+   if(current <= 0 && current >= 13)
    {
-       fprintf(stderr, "Invalid input\n");
-       exit(1);
+       printf("Invalid input\n");
+       return 1;
    }
 
    printf("How many hours have passed: ");
-   scanf("%c", temp2);
-   int passed = atoi(temp2);
+   scanf("%d", &temp2);
+   unsigned int passed = *(int*)&temp2;
 
-//    if(passed <= INT_MIN || passed >= INT_MAX)
-//    {
-//        fprintf(stderr, "Value is too large\n");
-//        return 2;
-//    }
+   if(passed <= INT_MIN && passed >= INT_MAX)
+   {
+       fprintf(stderr, "Invalid Interger\n");
+       return 2;
+   }
 
-   int timeNow = (current + passed);
-   printf("%d\n", timeNow);
+   int timeNow = current + passed;
+   
    int days = 0;
    int convertedTime;
    days = timeNow / 24;
