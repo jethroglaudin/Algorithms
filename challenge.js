e465ed7e8bdd23d55dc365bc72704809d4b2cf8b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-17 11:12:23 -0400

    updated

--- challenge.js
+++ challenge.js
@@ -10,9 +10,9 @@
 
 
 
-var now = new Date()
-var readlineSync = require('readline-sync');
-let currentYear = now.getFullYear()
+let now = new Date();
+const readlineSync = require('readline-sync');
+let currentYear = now.getFullYear();
 let currentMonth = now.getMonth() + 1;
 
 let userYear = parseInt(readlineSync.question("Enter a year "));
@@ -22,23 +22,23 @@ console.log(userYear);
 console.log(userMonth);
 
 if (!Number.isInteger(userMonth)){
-    console.log("Month but be in number format")
+    console.log("Month but be in number format");
     return 1;
 }
 
 if (userYear > currentYear){
     let futureYearlyDifference = userYear - currentYear;
-    let futureMonthDifference = Math.abs((userMonth - currentMonth) / 12)
+    let futureMonthDifference = Math.abs((userMonth - currentMonth) / 12);
     let futureTotal = futureYearlyDifference + futureMonthDifference;
 
-    console.log(`${futureTotal.toFixed(2)} years ahead`)
+    console.log(`${futureTotal.toFixed(2)} years ahead`);
     
 } else {
     let yearlyDifference = currentYear - userYear;
-    let monthlyDifference = Math.abs(((currentMonth - userMonth) / 12)) 
+    let monthlyDifference = Math.abs(((currentMonth - userMonth) / 12));
     let total = yearlyDifference + monthlyDifference;
     
-    console.log(`${total.toFixed(2)} year difference`)
+    console.log(`${total.toFixed(2)} year difference`);
 }
 
 
