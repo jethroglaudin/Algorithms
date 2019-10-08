10b288d8883e212f4b7a684c3129a6cd030b7499
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-08 13:23:33 -0400

    FCC challenge done with C

---
+++ findLongestWord.c
@@ -0,0 +1,33 @@
+#include<stdio.h> 
+#include<string.h>
+#include<stdlib.h> 
+#include <ctype.h>
+
+void LongestWord(char sen[]) {
+  
+  // code goes here
+  char prompt[] = "This is just wrong.";
+  printf("%c", prompt[0]);
+//   char tempArr[strlen(sen) + 1];
+//   int max = 0;
+//   for(int i = 0; i < strlen(sen); i++)
+//   {
+//      tempArr[i] = sen[i];  
+//   }
+
+// //   for (int j = 0; j < strlen(tempArr); j++)
+// //   {
+// //       if(max > strlen(tempArr[j]))
+// //   }
+//   printf("%s\n", tempArr);
+  // printf("%s", sen);
+
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  LongestWord("I love dogs");
+  return 0;
+    
+}
