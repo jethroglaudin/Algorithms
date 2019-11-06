bf30f83436386f57160e22d79cc8b5db0abc50db
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-06 17:59:40 -0500

    LeapYear challenge class work

---
+++ leapYear.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main (void)
+{
+    int yearInput;
+    bool isLeapYear;
+
+    printf("Enter a year: \n");
+    scanf("%i", &yearInput);
+    // printf("%i\n", yearInput);
+    
+    if(yearInput % 4 == 0 || yearInput % 4000 == 0)
+    {
+        isLeapYear = true;
+        printf("%s\n", "true");
+        return isLeapYear;  
+    }
+     else if (yearInput % 1000 == 0)
+    {
+        isLeapYear = false;
+        printf("%s\n", "false");
+        return isLeapYear;    
+    } else 
+    {
+        isLeapYear = false;
+        printf("%s\n", "false");
+        return isLeapYear;
+    }
+
+}
