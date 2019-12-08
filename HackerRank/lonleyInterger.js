ea907b51d8f73f14d14e0650a84078ee3c27659e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-12-07 21:52:34 -0500

    refactored code for better efficiency. Used bitwise operator XOR to find the lone interger

--- HackerRank/lonleyInterger.js
+++ HackerRank/lonleyInterger.js
@@ -1,15 +1,15 @@
 function lonelyinteger(a) {
-    let ans = 0;
-    // sort array 
-    let newArr = a.sort(); 
-    newArr.forEach((elem,index) => {
-        if(newArr[index + 1] !== elem && newArr[index - 1] !== elem)
-        ans = elem;
-        return ans;
-    })
-    console.log(ans);
-    return ans;
-
-    
-
+    // let ans = 0;
+    // // sort array 
+    // let newArr = a.sort(); 
+    // newArr.forEach((elem,index) => {
+    //     if(newArr[index + 1] !== elem && newArr[index - 1] !== elem)
+    //     ans = elem;
+    //     return ans;
+    // })
+    // console.log(ans);
+    //  return ans;
+   
+    // refactored solution using bitwise operator XOR
+    return a.reduce((x, y) => {return x ^= y});
 }
