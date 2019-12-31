fabbc4834a27f105a7dc3ce4142b123ded2cafd4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-12-30 21:27:46 -0500

    refactored my code using a more efficient approach. Used a hash table data structure.

--- firstRecurringCharacter2.js
+++ firstRecurringCharacter2.js
@@ -1,24 +1,22 @@
 function returningChar(string) {
-   let hashTable = {}; // create hash table
-    let ans = null;
+  let hashTable = {}; // create hash table
+  let ans = null;
 
-    for(let i in string){
-        if(!hashTable.hasOwnProperty(string[i])){
-            hashTable[string[i]] = 1;
-        }
-        else {
-            hashTable[string[i]] += 1;
-        }
-        if(hashTable[string[i]] > 1){
-            ans = string[i]
-            break;
-        }
+  for (let i in string) {
+    if (!hashTable.hasOwnProperty(string[i])) {
+      hashTable[string[i]] = 1;
+    } else {
+      hashTable[string[i]] += 1;
     }
-   console.log(hashTable)
-   console.log(ans)
+    if (hashTable[string[i]] > 1) {
+      ans = string[i];
+      break;
+    }
   }
- 
-  returningChar("ABCA");
-  returningChar("BCABA");
-  returningChar("ABCDEFG");
-  
+  console.log(hashTable);
+  console.log(ans);
+}
+
+returningChar("ABCA");
+returningChar("BCABA");
+returningChar("ABCDEFG");
