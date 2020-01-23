7eb86110412ccbac74e5fa6cfa2f7fcaf4c23d1a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-22 21:50:32 -0500

    inclass assignment

---
+++ Whiteboard Challenges/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace String_Calculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+
+        {
+            var calc = new calculate();
+            calc.Calculate("5 % 7");
+            Console.WriteLine(calc);
+        }
+    }
+}
---
+++ Whiteboard Challenges/calculate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace String_Calculator
+{
+    public class calculate
+    {
+        public int Calculate(string ex)
+        {
+
+            char[] seperate = { ' ' };
+            var arr = ex.Split(seperate);
+
+            var num1 = Int32.Parse(arr[0]);
+            var num2 = Int32.Parse(arr[2]);
+            var operation = arr[1];
+            var result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "*":
+                case "X":
+                case "x":
+                    result = num1 * num2;
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    break;
+   
+            }
+            return result;
+
+        }
+    }
+}
