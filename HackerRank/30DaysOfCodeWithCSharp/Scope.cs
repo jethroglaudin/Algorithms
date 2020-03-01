27820559e226a21d93e597488c0d7eef26f43075
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-01 18:44:23 -0500

    Day14: Scope completed with c#

---
+++ HackerRank/30DaysOfCodeWithCSharp/Scope.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+	// Add your code here
+    public Difference(int[] elements)
+    {
+        this.elements = elements;
+        this.maximumDifference = 0;
+    }
+    public int computeDifference()
+    {
+        Array.Sort(elements);
+        this.maximumDifference = Math.Abs(elements.First() - elements.Last());
+        return maximumDifference;
+    }
+
+} // End of Difference Class
+
+class Solution {
+    static void Main(string[] args) {
+        Convert.ToInt32(Console.ReadLine());
+        
+        int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+        
+        Difference d = new Difference(a);
+        
+        d.computeDifference();
+        
+        Console.Write(d.maximumDifference);
+    }
+}
