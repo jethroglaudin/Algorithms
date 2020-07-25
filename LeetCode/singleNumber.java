d126ae5148b416b7f71d0b4bd495904f7042ad54
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-07-25 19:41:12 -0400

    Memory Usage: 40.9 MB runtime 7ms

---
+++ LeetCode/singleNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int num : nums){
+            if(!map.containsKey(num)){
+               map.put(num, 1); 
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+       
+        Iterator it = map.entrySet().iterator();
+        int ans = 0;
+        
+        while(it.hasNext()){
+            Map.Entry pair = (Map.Entry)it.next();
+            if((int)pair.getValue() == 1){
+                ans = (int)pair.getKey();
+            }
+        }
+        return ans;
+        
+    }
+}
