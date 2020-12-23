64989a4a6bb32b8a09fb33cd9682b4255aec2744
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-22 23:15:14 -0500

    Runtime 1ms with a memory usage of 36.5 MB... Big O of O(nlogn) due to Java's Arrays.sort method

---
+++ LeetCode/height-checker.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+        int[] arr = heights.clone(); 
+        
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < heights.length; ++i){
+            if(heights[i] != arr[i]) {
+                ++count;
+            }
+        }
+        
+        return count;
+        
+    }
+}public class height-checker {
+    
+}
